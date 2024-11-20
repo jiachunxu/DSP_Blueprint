0e3e49fb7b20431f5775509b0da0421e8053ce31
jsw <954951185@qq.com>
2024-11-20 23:15:52 +0800

    fix

--- copy_blueprint.sh
+++ copy_blueprint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "开始复制"
+
+rm -rfv ./Blueprint ./DysonBluePrint
 # 将 win当前用户下的 Documents\Dyson Sphere Program\Blueprints 文件夹复制到当前文件夹下
 cp -r /c/Users/$(whoami)/Documents/Dyson\ Sphere\ Program/Blueprint/ .
 cp -r /c/Users/$(whoami)/Documents/Dyson\ Sphere\ Program/DysonBluePrint/ .
